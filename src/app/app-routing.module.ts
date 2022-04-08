9e7d37a0d9d7a6afa4737288e681334f424b1c76
unknown <jrodicio98@gmail.com>
2022-04-07 21:50:32 -0300

    Se agregó bootstrap y algunas correcciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'prefix'
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent, children: [
+    {path: '', component: HomeComponent },
+    { path: 'quien-soy', component: QuienSoyComponent },
+    { path: '**', component: ErrorComponent },
+    ]
   },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'quien-soy', component: QuienSoyComponent },
+   {path: 'quien-soy', component: QuienSoyComponent},
+  { path: '**', component: ErrorComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { ErrorComponent } from './error/error.component';
+import { AhorcadoComponent } from './ahorcado/ahorcado.component';
+import { MayorOMenorComponent } from './mayor-o-menor/mayor-o-menor.component';
+import { PreguntadosComponent } from './preguntados/preguntados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    QuienSoyComponent
+    QuienSoyComponent,
+    ErrorComponent,
+    AhorcadoComponent,
+    MayorOMenorComponent,
+    PreguntadosComponent
   ],
   imports: [
     BrowserModule,
